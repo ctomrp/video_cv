f2edf28505411faa878aaf0f16fb88475d320093
ctomrp <cristian.rico.palma@gmail.com>
2023-12-11 16:12:06 -0300

    index

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function startAudio() {
 //lights
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-}
+  }
 
-function showHiredText(words, currentIndex) {
+  function showHiredText(words, currentIndex) {
     currentIndex = currentIndex % words.length;
 
     var hiredText = document.createElement('div');
@@ -23,12 +23,14 @@ function showHiredText(words, currentIndex) {
     var screenWidth = window.innerWidth;
     var screenHeight = window.innerHeight;
 
+    // Tamaño del texto
     var textWidth = 7 * currentWord.length;
     var textHeight = 7;
 
     var randomX = getRandomNumber(0, screenWidth - textWidth);
     var randomY = getRandomNumber(0, screenHeight - textHeight);
 
+    // Verificar y ajustar la posición para asegurar que esté dentro del área del DOM
     randomX = Math.max(0, Math.min(randomX, screenWidth - textWidth));
     randomY = Math.max(0, Math.min(randomY, screenHeight - textHeight));
 
@@ -38,18 +40,19 @@ function showHiredText(words, currentIndex) {
     document.body.appendChild(hiredText);
 
     setTimeout(function () {
-        hiredText.style.display = 'block';
-        setTimeout(function () {
-            hiredText.style.display = 'none';
-            document.body.removeChild(hiredText);
-        }, 490);
+      hiredText.style.display = 'block';
+      setTimeout(function () {
+        hiredText.style.display = 'none';
+        document.body.removeChild(hiredText);
+      }, 490);
     }, 0);
 
     return currentIndex + 1;
-}
+  }
 
-var wordList = ['Awesome', 'Success', 'Innovation', 'Teamwork', 'Punctual', 'Hired! ;)'];
+  var wordList = ['Hired', 'Awesome', 'Success', 'Innovation', 'Teamwork'];
 
-setInterval(function () {
+  var currentIndex = 0;
+  setInterval(function () {
     currentIndex = showHiredText(wordList, currentIndex);
-}, 490);
+  }, 490);
